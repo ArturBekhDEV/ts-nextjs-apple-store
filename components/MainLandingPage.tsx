3426c54dcfd5014771bfbf9adb254bdb700c44e0
ArturBekhDEV <artbekhDEV@gmail.com>
2022-11-29 15:16:50 +0200

    1

---
+++ components/MainLandingPage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const MainLandingPahe = () => {
+  return (
+    <section>
+      <div>
+        <h1 className="space-y-3 text-5xl font-semibold tracking-wide">
+          <span className="block bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent">
+            Powered by Innovations.
+          </span>
+          <span className="block">Changing world</span>
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default MainLandingPahe;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
+import MainLandingPage from "../components/MainLandingPage";
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +12,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <main>
+        <MainLandingPage/>
+      </main>
     </div>
   );
 };
