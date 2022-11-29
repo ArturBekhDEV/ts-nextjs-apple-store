2b73258ea28d3aab42e95823856aee7a1b6be614
ArturBekhDEV <102412173+ArturBekhDEV@users.noreply.github.com>
2022-11-29 17:56:21 +0200

    1

--- components/MainLandingPage.tsx
+++ components/MainLandingPage.tsx
@@ -3,8 +3,8 @@ import Spline from "@splinetool/react-spline";
 
 const MainLandingPahe = () => {
   return (
-    <section className="sticky top-0 mx-auto h-screen max-w-[1350px] items-center justify-between px-8 lg:flex align-middle">
-      <div className="space-y-4">
+    <section className="sticky top-0 mx-auto h-screen max-w-[1350px] items-center justify-between px-8 lg:flex">
+      <div className="space-y-4 md:text-center">
         <h1 className="space-y-5 text-6xl font-bold lg:text-6xl xl:text-7xl">
           <span className="block bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent">
             Powered.
@@ -17,7 +17,7 @@ const MainLandingPahe = () => {
           <div>{/* <Button title="Buy now" /> */}</div>
         </h1>
       </div>
-      <div>
+      <div className="relative hidden h-[450px] w-[450px] transition-all duration-500 md:inline lg:h-[650px] lg:w-[600px]">
         <Spline scene="https://prod.spline.design/KKoTrCA0O4mcYlrG/scene.splinecode" />
       </div>
     </section>
