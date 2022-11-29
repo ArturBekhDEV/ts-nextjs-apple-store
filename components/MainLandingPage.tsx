85088e289c22d0931bc3eae7eb7a13a46a1e0025
ArturBekhDEV <102412173+ArturBekhDEV@users.noreply.github.com>
2022-11-29 16:19:28 +0200

    1

--- components/MainLandingPage.tsx
+++ components/MainLandingPage.tsx
@@ -2,14 +2,21 @@ import React from "react";
 
 const MainLandingPahe = () => {
   return (
-    <section>
-      <div>
-        <h1 className="space-y-3 text-5xl font-semibold tracking-wide">
+    <section className="sticky top-0 mx-auto flex h-screen max-w-[1350px] items-center justify-between px-8">
+      <div className="space-y-4">
+        <h1 className="space-y-5 text-6xl font-bold lg:text-6xl xl:text-7xl">
           <span className="block bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent">
-            Powered by Innovations.
+            Powered.
           </span>
-          <span className="block">Changing world</span>
+          <span className="block bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent">
+            Innovations.
+          </span>
+          <span className="block">Changing world.</span>
         </h1>
+        <div>
+          {/* <Button title="Buy now" /> */}
+          <a className="link">Learn More</a>
+        </div>
       </div>
     </section>
   );
