f417bb570c34e6e5b7fc59d7ae4e6543de5c4b76
ArturBekhDEV <102412173+ArturBekhDEV@users.noreply.github.com>
2022-12-23 15:23:22 +0200

    updated and fixed margin of 3d element

--- components/MainLandingPage.tsx
+++ components/MainLandingPage.tsx
@@ -4,8 +4,8 @@ import Button from "./Button";
 
 const MainLandingPahe = () => {
   return (
-    <section className="sticky top-0 mx-auto h-screen max-w-[1350px] items-center justify-between px-8 text-center lg:flex lg:text-left">
-      <div className="space-y-4">
+    <section className="sticky top-0 mx-auto h-screen max-w-[1350px] items-center justify-between px-8 lg:flex text-center  lg:text-left">
+      <div className="space-y-8">
         <h1 className="space-y-3 text-5xl font-semibold tracking-wide lg:text-6xl xl:text-7xl">
           <span className="block bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent">
             Powered
@@ -15,16 +15,18 @@ const MainLandingPahe = () => {
           </span>
           <span className="block font-bold">Driven By Values</span>
         </h1>
+
         <div className="space-x-8">
           <Button title="Buy Now" />
           <a className="link">Learn More</a>
         </div>
       </div>
 
-      <div className="relative hidden transition-all duration-500 sm:inline lg:mx-100">
-        <div className="content-center">
-          <Spline scene="https://prod.spline.design/KKoTrCA0O4mcYlrG/scene.splinecode" />
-        </div>
+      <div className="relative hidden transition-all duration-500 sm:block ">
+        <Spline
+          className="mx-auto"
+          scene="https://prod.spline.design/KKoTrCA0O4mcYlrG/scene.splinecode"
+        />
       </div>
     </section>
   );
