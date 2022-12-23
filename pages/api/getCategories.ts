52a0f71fd2fbcbdf35bdb567417e1107cdd5e8cd
ArturBekhDEV <102412173+ArturBekhDEV@users.noreply.github.com>
2022-12-23 14:02:26 +0200

    updated icons in sanity

--- pages/api/getCategories.ts
+++ pages/api/getCategories.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { groq } from "next-sanity";
+import { sanityClient } from "../../sanity";
+const query = groq`*[_type=='category']{
+  _id,
+  ...
+}`;
 
 type Data = {
   categories: [Category]; // from typing.d.ts
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  //   const categories = await sanityClient.fetch()
+  const categories = await sanityClient.fetch(query);
 }
