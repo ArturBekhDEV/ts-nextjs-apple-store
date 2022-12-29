bd85279ae9d780dbe64d6c7a0f4e1ea78dc56ab9
ArturBekhDEV <102412173+ArturBekhDEV@users.noreply.github.com>
2022-12-29 13:54:32 +0200

    ss

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,11 +1,24 @@
+import Image from "next/image";
 import React from "react";
+import { urlFor } from "../sanity";
 
 interface Props {
   product: Product;
 }
 
 const Product = ({ product }: Props) => {
-  return <div>Product</div>;
+  console.log(product);
+  return (
+    <div>
+      <div>
+        <Image
+          src={urlFor(product.image[0]).url()}
+          layout="fill"
+          objectFit="contain"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Product;
