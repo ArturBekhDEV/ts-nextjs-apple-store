ff3650faa96d4393a633882203ec9fa219375db6
ArturBekhDEV <102412173+ArturBekhDEV@users.noreply.github.com>
2022-12-29 14:18:17 +0200

    ss

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,3 +1,4 @@
+import { ShoppingBagIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import React from "react";
 import { urlFor } from "../sanity";
@@ -9,13 +10,22 @@ interface Props {
 const Product = ({ product }: Props) => {
   console.log(product);
   return (
-    <div>
-      <div>
+    <div className="flex h-fit w-[320px] select-none flex-col space-y-3  bg-[#35383C] p-8 md:h-[500px] md:w-[400px] md:p-10">
+      <div className="relative h-64 w-full md:h-72">
         <Image
           src={urlFor(product.image[0]).url()}
           layout="fill"
           objectFit="contain"
         />
+      </div>
+      <div className="flex flex-1 items-center justify-between space-x-4">
+        <div className="space-y-2 text-xl text-white md:text-2xl">
+          <p>{product.title}</p>
+          <p>{product.price} $</p>
+        </div>
+        <div className="flex h-16 w-16 flex-shrink-0 cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 md:h-[50px] md:w-[50px]">
+          <ShoppingBagIcon className="h-8 w-8 text-white" />
+        </div>
       </div>
     </div>
   );
